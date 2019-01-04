aeb68ebaaa87e387b9cd92d2c7b9b9579bf6a378
Vlad Sliusar <31670686+vladsliusar@users.noreply.github.com>
2019-01-03 20:47:14 -0800

    DMS/DD Converter
    This program collects user input of Decimal Degrees (DD) or Degrees/Minutes/Seconds (DMS), converts it to the other form, and reports the converted value.

---
+++ Python Functions and Containers/dmsORdd.py
@@ -0,0 +1,56 @@
+"""
+    This program collects user input of DD or DMS, converts it to the other form,
+    and reports the converted value.
+"""
+        
+# Import math module.
+import math
+# Define function dms2dd that performs necessary calculations to convert DMS to DD.
+def dms2dd((degrees, minutes, seconds)):
+    # assign values to variables
+    degrees = number_dmsORdd[0]
+    minutes = number_dmsORdd[1]
+    seconds = number_dmsORdd[2]
+    # If loop ensures that negative and positive input values of DMS are calculated appropriately.
+    if degrees >0:
+        DD = degrees+minutes/60+seconds/3600
+    else:
+        DD = degrees-minutes/60-seconds/3600
+    print "Its DD form is",round(DD,4),"." # print message to the screen
+# Define function dms2dd that performs necessary calculations to convert DD to DMS.
+def dd2dms(degrees):
+    D = int(degrees) # calculate degrees value using int function
+    # calculations for minutes:
+    DM = degrees - D
+    MS = DM * 60
+    M = int(MS) # calculate final minutes value using int function
+    # calculations for seconds:
+    SM = MS - M
+    S = int(SM*60) # calculate final seconds value using int function
+    # If loop ensures that negative input values of DD are calculated appropriately.
+    if degrees < 0:
+        M = M *(-1)
+        S = S *(-1)
+    print "Its DMS form is",D,M,S,"." # print message to the screen
+
+# Assign raw_input function to variable dmsORdd, collect user input as a string value.
+dmsORdd = raw_input("Please enter a latitude or longitute value in DMS or DD format. >")
+# Split function creates a list from user input values.
+dmsORdd.split()
+# Convert each item in the list to float type, 
+# assign the list to the variable number_dmsORdd.
+number_dmsORdd = [float(i) for i in dmsORdd.split(',')]
+# If loop determines if the user input is DD or DMS
+# and directs to the appropriate function for conversion.
+if number_dmsORdd == number_dmsORdd[:1]: # if one item in the list
+    number_dmsORdd = float (number_dmsORdd[0])
+    print "The input value is in DD form." # print message to the screen
+    # Call dd2dms fucntion.
+    dd2dms(number_dmsORdd)
+elif number_dmsORdd == number_dmsORdd[0:3]: # if 3 items in the list
+    print "The input value is in DMS form." # print message to the screen
+    # Call dms2dd fucntion.
+    dms2dd(number_dmsORdd)
+
+
+
